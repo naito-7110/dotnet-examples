5a546f93c239ffdb4cfab607f6e30e0acd081b30
naito-7110 <shige.7110.330@gmail.com>
2026-05-24 15:23:00 +0900

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "dotnet devShell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        dotnetCorePackages.sdk_10_0
+        roslyn-ls
+      ];
+    };
+  };
+}
